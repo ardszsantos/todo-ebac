84850490e5d7ce3a3e745f757d01236b14d1ffbd
ardszsantos <lokgamer01@gmail.com>
2025-08-07 17:31:08 -0300

    ebac

---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,42 @@
+import Tarefa from '../../components/Tarefa'
+import * as S from './styles'
+
+const ListaDeTarefas = () => (
+  <S.Container>
+    <p>2 tarefas marcadas como: categoria e termo</p>
+    <ul>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+    </ul>
+  </S.Container>
+)
+
+export default ListaDeTarefas
