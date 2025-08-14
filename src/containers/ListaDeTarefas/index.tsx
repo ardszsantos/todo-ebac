9d8e70162fadbf79b7b90207ce47f2c653daa09e
ardszsantos <lokgamer01@gmail.com>
2025-08-14 14:47:23 -0300

    foi

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,43 +1,27 @@
+import { useSelector } from 'react-redux'
 import Tarefa from '../../components/Tarefa'
 import * as S from './styles'
+import { RootReducer } from '../../store'
 
-const tarefas = [
-  {
-    titulo: 'Estudar',
-    descricao: 'Ver',
-    prioridade: 'importante',
-    status: 'pendente'
-  },
-  {
-    titulo: 'Pagar',
-    descricao: 'Baixar',
-    prioridade: 'urgente',
-    status: 'concluida'
-  },
-  {
-    titulo: 'ir',
-    descricao: 'fazer',
-    prioridade: 'importante',
-    status: 'pendente'
-  }
-]
-
-const ListaDeTarefas = () => (
-  <S.Container>
-    <p>2 tarefas marcadas como: categoria e termo</p>
-    <ul>
-      {tarefas.map((t) => (
-        <li key={t.titulo}>
-          <Tarefa
-            descricao={t.descricao}
-            titulo={t.titulo}
-            status={t.status}
-            prioridade={t.prioridade}
-          />
-        </li>
-      ))}
-    </ul>
-  </S.Container>
-)
+const ListaDeTarefas = () => {
+  const { tarefas } = useSelector((state: RootReducer) => state)
+  return (
+    <S.Container>
+      <p>2 tarefas marcadas como: categoria e termo</p>
+      <ul>
+        {tarefas.map((t) => (
+          <li key={t.titulo}>
+            <Tarefa
+              descricao={t.descricao}
+              titulo={t.titulo}
+              status={t.status}
+              prioridade={t.prioridade}
+            />
+          </li>
+        ))}
+      </ul>
+    </S.Container>
+  )
+}
 
 export default ListaDeTarefas
