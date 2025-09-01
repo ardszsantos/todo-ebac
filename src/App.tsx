038c2f996e3d5f534b823ac7773918dfc562285b
ardszsantos <lokgamer01@gmail.com>
2025-09-01 14:49:25 -0300

    react browser dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 import React from 'react'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import EstiloGlobal, { Container } from './styles'
 import BarraLateral from './containers/BarraLateral'
 import ListaDeTarefas from './containers/ListaDeTarefas'
 import { Provider } from 'react-redux'
 import store from './store'
+import Home from './pages/Home'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/novo',
+    element: <h1>nova tarefas</h1>
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal />
       <Container>
-        <BarraLateral />
-        <ListaDeTarefas />
+        <RouterProvider router={rotas} />
       </Container>
     </Provider>
   )
